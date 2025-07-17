70ba15a603705db0a87d60420a07047bf51e47e9
Yurii Posin <yuriiposin@gmail.com>
2025-07-17 13:11:21 +0000

    Set alieases

--- init/init.sh
+++ init/init.sh
@@ -1,3 +1,16 @@
-cat promt >> ~/.bashrc
+reinit(){
+  reset
+  exec sudo --login --user "$USER" /bin/sh -c "cd '$PWD'; exec '$SHELL' -l"
+}
 
+setpromt(){
+  cat promt >> ~/.bashrc
+}
 
+setaliases(){
+  cat aliases >> ~/.bashrc
+}
+
+setpromt
+setaliease
+reinit
